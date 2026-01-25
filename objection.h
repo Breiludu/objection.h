42ca33710fbdf4f3b088d230324bea01a1d01776
Breiludu <breiludu@gmail.com>
2026-01-25 20:34:04 +0100

    Colourful prints with ANSI codes

--- objection.h
+++ objection.h
@@ -1,7 +1,7 @@
 /*
  * objection.h
  *
- * Version: 2.0.0
+ * Version: 2.1.0-alpha.1
  *
  * Copyright 2026 (c) Breixo Luna Durán
  *
@@ -11,6 +11,18 @@
 
 #include <stdio.h>
 #include <stdint.h>
+
+// Colors for printing in terminal
+#define __COLOR_RED     "\x1b[31m"
+#define __COLOR_GREEN   "\x1b[32m"
+#define __COLOR_YELLOW  "\x1b[33m"
+#define __COLOR_BLUE    "\x1b[34m"
+#define __COLOR_MAGENTA "\x1b[35m"
+#define __COLOR_CYAN    "\x1b[36m"
+#define __COLOR_RESET   "\x1b[0m"
+
+#define __STYLE_BOLD         "\033[1m"
+#define __STYLE_NO_BOLD      "\033[22m"
 
 typedef void(*TrialFn)(void);
 
@@ -31,14 +43,19 @@ static char **__curr_trial_name;
 // True if last executed trial has failed
 static int __has_failed;
 
+// --- Exposed macros for user api ---
+
 /// Pass in a statement that should evaluate to true.
 #define OBJECTION(statement) do {                               \
     __total_objected++;                                         \
     if(!(statement)) {                                          \
         __total_failed++;                                       \
         __has_failed = 1;                                       \
-        printf("[FAIL]     OBJECTION(%s)\n", #statement);       \
-        printf("[INFO]       In trial %s -> (%s, %d)\n",        \
+        __print_fail_prefix();                                  \
+        printf("    OBJECTION(%s)\n", #statement);              \
+        __print_info_prefix();                                  \
+        printf("      In trial " __STYLE_BOLD "%s"              \
+            __STYLE_NO_BOLD "-> (%s, %d)\n",                    \
             *__curr_trial_name, __FILE__, __LINE__);            \
     }                                                           \
 } while(0)
@@ -55,6 +72,20 @@ static int __has_failed;
                                                                \
     static void __trial_##name(void)
 
+// --- Internal use functions ---
+
+static inline void __print_fail_prefix() {
+    printf(__STYLE_BOLD "[" __COLOR_RED "FAIL" __COLOR_RESET "] " __STYLE_NO_BOLD);
+}
+
+static inline void __print_pass_prefix() {
+    printf(__STYLE_BOLD "[" __COLOR_GREEN "PASS" __COLOR_RESET "] " __STYLE_NO_BOLD);
+}
+
+static inline void __print_info_prefix() {
+    printf(__STYLE_BOLD "[" __COLOR_YELLOW "INFO" __COLOR_RESET "] " __STYLE_NO_BOLD);
+}
+
 // Main function that calls all TRIALs
 int main(void) {
     printf("[====] objection.h unit testing\n");
@@ -66,12 +97,13 @@ int main(void) {
         (*trial)();
 
         if(!__has_failed) {
-            printf("[PASS] Trial %s passed\n", *__curr_trial_name);
+            __print_pass_prefix();
+            printf("Trial " __STYLE_BOLD "%s" __STYLE_NO_BOLD " passed\n", *__curr_trial_name);
         }
         printf("[----]\n");
         __curr_trial_name++;
         __has_failed = 0;
     }
-    printf("[====] Objections: %u | Failed: %u\n", __total_objected, __total_failed);
+    printf("[====] " __STYLE_BOLD "Objections: %u | Failed: %u\n" __STYLE_NO_BOLD, __total_objected, __total_failed);
     return 0;
 }
